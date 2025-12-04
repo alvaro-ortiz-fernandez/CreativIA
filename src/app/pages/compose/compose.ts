a724e3d817b4b122261382cfd987d0cf136ee89f
alvaro-ortiz-fernandez <40672795+alvaro-ortiz-fernandez@users.noreply.github.com>
2025-12-05 00:22:57 +0100

    Implementando avisos de carga

--- src/app/pages/compose/compose.ts
+++ src/app/pages/compose/compose.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { GeminiService } from '../../services/gemini';
+import { Text2ColorService } from '../../services/text2-color';
+import { TheColorAPIService } from '../../services/the-color-api';
+import { BtnSpinner } from '../../components/btn-spinner/btn-spinner';
 
 @Component({
   selector: 'app-compose',
-  imports: [RouterLink],
+  imports: [RouterLink, BtnSpinner],
   templateUrl: './compose.html',
   styleUrl: './compose.scss',
 })
 export class Compose {
 
+  geminiService: GeminiService = inject(GeminiService);
+  text2ColorService: Text2ColorService = inject(Text2ColorService);
+  theColorAPIService: TheColorAPIService = inject(TheColorAPIService);
 }
--- src/app/pages/library/library.ts
+++ src/app/pages/library/library.ts
@@ -1,27 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { FirebaseService } from '../../services/firebase';
+import { Observable } from 'rxjs';
+import { Spinner, SpinnerSize } from '../../components/spinner/spinner';
 
 @Component({
   selector: 'app-library',
-  imports: [],
+  imports: [AsyncPipe, Spinner],
   templateUrl: './library.html',
   styleUrl: './library.scss',
 })
 export class Library {
 
-  colorPalettes: ColorPalette[] = [
-    {
-      id: "1",
-      baseColor: '#CCCCCC',
-      scheme: "Tetrádico",
-      description: "Esquema de color seco.",
-      context: "A emplear en la creación de combinaciones vibrantes.",
-      interpretation: "El color gris suave transmite neutralidad y equilibrio, proporcionando una base versátil que puede complementar una amplia gama de colores.",
-      colors: [
-        { name: "Gris suave", hex: "#CCCCCC", r: 204, g: 204, b: 204 },
-        { name: "Azul cielo", hex: "#99CCFF", r: 153, g: 204, b: 255 },
-        { name: "Naranja cálido", hex: "#FFCC99", r: 255, g: 204, b: 153 },
-        { name: "Verde menta", hex: "#99FFCC", r: 153, g: 255, b: 204 }
-      ]
-    }
-  ];
+  SpinnerSize = SpinnerSize; // Exponer el enum al template
+
+  private firebaseService: FirebaseService = inject(FirebaseService);
+  colorPalettes: Observable<ColorPalette[]> = this.firebaseService.getColorPalettes();
 }
